1b32ca321b9de2fab1522efcc85b2688621086fe
Thiago Silva <thi4go.marqu3s@gmail.com>
2024-04-30 14:35:22 -0300

    teste

---
+++ forca.h
@@ -0,0 +1,16 @@
+#ifndef FORCA_H
+#define FORCA_H
+
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+int acertou();
+int enforcou();
+int jachutou(char letra);
+void adicionapalavra();
+int chuteserrados();
+
+#endif //FORCA_H
